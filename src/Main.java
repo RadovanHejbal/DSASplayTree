656fd2a833da92f400183e975983a522f6d99fd9
Slepsy <hejbalradovan@gmail.com>
2023-03-13 10:51:22 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
                 case 1:
                     System.out.print("Number:");
                     tree.rootNode = tree.insertNode(scan.nextInt(), tree.rootNode);
-                    System.out.println(tree.rootNode.height);
+                    System.out.println(tree.rootNode.element);
                     break;
                 case 2:
                     System.out.print("Number:");
@@ -27,21 +27,17 @@ public class Main {
                     break;
                 case 3:
                     System.out.print("Number:");
-                    tree.searchNode(scan.nextInt(), tree.rootNode);
+                    tree.rootNode = tree.searchNode(scan.nextInt(), tree.rootNode);
+                    System.out.println(tree.rootNode.element);
                     break;
             }
         }while (choice != 0);
-    }
-
-    public static void changeNode(Node change) {
-        change.element += 1;
     }
 }
 
 class Node {
     Node leftNode = null;
     Node rightNode = null;
-    int height = 0;
     int element;
 
     public Node(int element) {
@@ -57,24 +53,34 @@ class NodeTree {
     public Node insertNode(int number, Node node) {
         if(node == null) {
             node = new Node(number);
-            System.out.println("Number " + number +" inserted!");
-        } else if (node.element > number) {
-            node.leftNode = insertNode(number, node.leftNode);
-        } else if (node.element < number) {
-            node.rightNode = insertNode(number, node.rightNode);
-        } else {
-            System.out.println("Number you entered is already in the tree!");
+            System.out.println("Number " + number + " inserted!");
             return node;
         }
-        node = balanceNode(node, number);
 
-        return node;
+        node = splaying(node, number);
+
+        if (node.element == number) return node;
+
+        Node inserting = new Node(number);
+
+        if(node.element < number) {
+            inserting.leftNode = node;
+            inserting.rightNode = node.rightNode;
+            node.rightNode = null;
+        }
+        else if(node.element > number) {
+            inserting.rightNode = node;
+            inserting.leftNode = node.leftNode;
+            node.leftNode = null;
+        }
+
+        return inserting;
     }
 
     public Node deleteNode(int number, Node node) {
         if(node == null) {
-            System.out.println("There is not such number in th tree!");
-            return node;
+            System.out.println("There is not such number in the tree!");
+            return null;
         } else if (node.element == number) {
             if(node.rightNode != null && node.leftNode != null) {
                 Node nodeHelp = node.rightNode;
@@ -96,50 +102,23 @@ class NodeTree {
             node.rightNode = deleteNode(number, node.rightNode);
         }
 
-        node.height = getMax(getHeight(node.rightNode), getHeight(node.leftNode)) + 1;
-        int balance = getBalance(node);
-        if(balance > 1) {
-            int leftBalance = getBalance(node.leftNode);
-            if(leftBalance >= 0) {
-                return rightRotation(node);
-            } else if(leftBalance < 0) {
-                node.leftNode = leftRotation(node.leftNode);
-                return rightRotation(node);
-            }
-        } else if(balance < -1) {
-            int rightBalance = getBalance(node.rightNode);
-            if(rightBalance <= 0) {
-                return leftRotation(node);
-            }else if(rightBalance < 0) {
-                node.rightNode = rightRotation(node.rightNode);
-                return leftRotation(node);
+        return node;
+    }
+
+    public Node searchNode(int number, Node node) {
+        if(node ==  null) {
+            System.out.println("Tree is empty!");
+        }
+        else {
+            node = splaying(node, number);
+            if(node.element == number) {
+                System.out.println("Number is in the tree and now became root");
+            }else {
+                System.out.println("There is no such number in the tree");
             }
         }
 
         return node;
-    }
-
-    public void searchNode(int number, Node node) {
-        if(node ==  null) {
-            System.out.println("There is no such number in the tree!");
-        } else if (node.element == number) {
-            System.out.println("I found this number in the tree");
-        } else if (node.element > number) {
-            searchNode(number, node.leftNode);
-        } else {
-            searchNode(number, node.rightNode);
-        }
-    }
-
-    public int getHeight(Node x) {
-        if(x == null) return -1;
-        else return x.height;
-    }
-
-    public int getBalance(Node x) { return getHeight(x.leftNode) - getHeight(x.rightNode); }
-    public int getMax(int x, int y) {
-        if(x>y) return x;
-        return y;
     }
 
     public Node leftRotation(Node x) {
@@ -147,8 +126,6 @@ class NodeTree {
         Node z = x.rightNode.leftNode;
         y.leftNode = x;
         x.rightNode = z;
-        x.height = getMax(getHeight(x.leftNode), getHeight(x.rightNode)) + 1;
-        y.height = getMax(getHeight(y.leftNode), getHeight(y.rightNode)) + 1;
         return y;
     }
 
@@ -157,28 +134,50 @@ class NodeTree {
         Node z = x.leftNode.rightNode;
         y.rightNode = x;
         x.leftNode = z;
-        x.height = getMax(getHeight(x.leftNode), getHeight(x.rightNode)) + 1;
-        y.height = getMax(getHeight(y.leftNode), getHeight(y.rightNode)) + 1;
         return y;
     }
 
-    public Node balanceNode(Node x, int number) {
-        x.height = getMax(getHeight(x.leftNode), getHeight(x.rightNode)) + 1;
-        int balance = getBalance(x);
-        if(balance < -1) {
-            if(x.rightNode.element<number) return leftRotation(x);
-            else if(x.rightNode.element>number) {
-                x.rightNode = rightRotation(x.rightNode);
+    public Node splaying(Node x, int el) {
+        if(x == null || x.element == el) {
+            return x;
+        }
+
+        if(x.element < el) {
+            if (x.rightNode == null) return x;
+
+            if(x.rightNode.element < el) {
+                x.rightNode.rightNode = splaying(x.rightNode.rightNode, el);
+                x = leftRotation(x);
+            }
+            else if(x.rightNode.element > el) {
+                x.rightNode.leftNode = splaying(x.rightNode.leftNode, el);
+                if(x.rightNode.leftNode != null) x.rightNode = rightRotation(x.rightNode);
+            }
+
+            if(x.rightNode != null) {
                 return leftRotation(x);
             }
-        }else if(balance > 1) {
-            if(x.leftNode.element>number) return rightRotation(x);
-            else if(x.rightNode.element<number) {
-                x.leftNode = leftRotation(x.leftNode);
+
+            return x;
+        }
+        else {
+            if(x.leftNode == null) return x;
+
+            if(x.leftNode.element < el) {
+                x.leftNode.rightNode = splaying(x.leftNode.rightNode, el);
+                if (x.leftNode.rightNode != null) x.leftNode = leftRotation(x.leftNode);
+            }
+            else if(x.leftNode.element > el) {
+                x.leftNode.leftNode = splaying(x.leftNode.leftNode, el);
+                x = rightRotation(x);
+            }
+
+            if(x.leftNode != null) {
                 return rightRotation(x);
             }
+
+            return x;
         }
-        return x;
     }
 }
 
